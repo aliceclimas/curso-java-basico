7a2f9e9d3521bc6477be37859797a3905724b0cc
aliceclimas <alicecristinalima31@gmail.com>
2025-05-27 08:23:56 -0300

    aula 37 finalizada

---
+++ aula37/Professor.java
@@ -0,0 +1,33 @@
+public class Professor extends Pessoa{
+    
+    private double salario;
+    private String nomeCurso;
+
+    // métodos getters e setters
+   
+    
+    // outros métodos
+    public double CalcularSalarioLiquido(){
+        return 0;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+
+    
+
+    
+}
---
+++ aula37/Teste.java
@@ -0,0 +1,26 @@
+public class Teste {
+    public static void main(String[] args) {
+
+        //Aluno aluno = new Aluno(); //tenho acesso aos atributos da subclasse e superclasse
+
+        //Professor professor = new Professor();
+
+        //Pessoa pessoa = new Pessoa();
+
+        //aluno.setNome("Maria");
+
+        //Pessoa aluno = new Aluno();// acesso somente dos atributos da classe pessoa
+        //aluno.get
+
+        Pessoa professor = new Professor();
+
+        Pessoa pessoa = new Pessoa();
+
+        Pessoa aluno = new Aluno(); // só tenho acesso aos atributos de pessoa -> superclasse 
+        
+
+        
+
+    }
+    
+}
