ed75d1d4bc5e428b681f0fdb1a1957b5d6b5ca62
aliceclimas <alicecristinalima31@gmail.com>
2025-05-28 09:14:47 -0300

    aula 45

---
+++ aula45/Teste.java
@@ -0,0 +1,32 @@
+public class Teste {
+
+    public static void main(String[] args) {
+
+        // Aluno aluno = new Aluno();
+        // Pessoa alunoPessoa = aluno; //upcasting
+
+        // Pessoa aluno2 = (Pessoa) new Aluno();
+
+        // Pessoa aluno3 = new Pessoa();
+        // Aluno aluno4 = aluno; // or Aluno aluno4 = (Aluno) aluno3
+
+        Pessoa pessoa = new Pessoa();
+        Aluno aluno = new Aluno();
+        Professor prof = new Professor();
+
+        if (pessoa instanceof Pessoa){
+            System.out.println("é do tipo Pessoa");
+        }
+
+        if (aluno instanceof Aluno){
+            System.out.println("é do tipo Pessoa");
+        }
+        if (prof instanceof Professor){
+            System.out.println("é do tipo Pessoa");
+        }
+        
+    }
+
+   
+    
+}
